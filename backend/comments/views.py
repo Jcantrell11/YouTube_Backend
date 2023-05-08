50c20346298e8118884cec0d9d6ee15a642a3038
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-05-08 14:33:36 -0400

    created Comment model in comments app + created Reply model in replies app + added comments app to settings and migrated to mySQL

---
+++ backend/comments/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Car
+from .serializers import CarSerializer
+# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
